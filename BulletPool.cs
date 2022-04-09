48ae8fc5eaf1f36b515f57d658aef603111e921e
rajk7 <50164635+rajk7@users.noreply.github.com>
2022-04-09 20:31:00 +0530

    Create BulletPool.cs

---
+++ BulletPool.cs
@@ -0,0 +1,35 @@
+        public GameObject _objectToPoolPrefab;
+        public GameObject _bulletPrefab;
+        public int _poolSize;
+
+        private List<GameObject> _bulletPool;
+
+        public void InitPool()
+        {
+            _bulletPool = new List<GameObject>();
+            for (int i = 0; i < _poolSize; i++)
+            {
+                GameObject newBullet = Instantiate(_objectToPoolPrefab);
+                _bulletPool.Add(newBullet);
+                _bulletPool[i].SetActive(false);
+            }
+        }
+
+        public GameObject GetObjFromPool(Vector3 targetPos, Quaternion rotation)
+        {
+            GameObject newBullet = _bulletPool[_bulletPool.Count - 1 ];
+            newBullet.SetActive(true);
+            newBullet.transform.position = targetPos;
+            newBullet.transform.rotation = rotation;
+            _bulletPool.RemoveAt(_bulletPool.Count - 1);
+
+            return newBullet;
+        }
+
+        public void ReturnObjFromPool(GameObject go)
+        {
+            go.SetActive(false);
+            _bulletPool.Add(go);
+
+
+        }
